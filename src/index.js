b22f0caadc9ec897d24caf353491e251c6e136cc
mavluda97 <mavluda97@gmail.com>
2019-07-06 21:15:11 +0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,29 @@
+module.exports = function check(str, bracketsConfig) {
+  // your solution
+
+   if (str.length <= 1){
+   	return false;}
+
+  let matchingOpeningBracket, ch
+  let stack = []
+
+  let openingBrackets = ['[', '{', '(']
+  let closingBrackets = [']', '}', ')']
+
+  for (let i = 0; i < str.length; i++) {
+    ch = str[i]
+
+    if (closingBrackets.indexOf(ch) > -1) {
+      matchingOpeningBracket = openingBrackets[closingBrackets.indexOf(ch)]
+      if (stack.length == 0 || (stack.pop() != matchingOpeningBracket)) {
+        return false
+      }
+    } else {
+      stack.push(ch)
+    }
+  }
+
+  return (stack.length == 0)
+
+
+};
